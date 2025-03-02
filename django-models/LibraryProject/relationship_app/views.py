8cfa0d25f05ad0026a6fbf18713beb606de2783b
Norwicklee <nomalisa.n1@gmail.com>
2025-03-02 21:42:49 +0200

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+#from .models import Book
+
+#def list_books(request):
+   # books = Book.objects.all()
+    #return render(request, 'list_books.html', {'books': books})
+
+from django.http import HttpResponse
 from .models import Book
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    text_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(text_list, content_type="text/plain")
 
 from django.views.generic import DetailView
 from .models import Library
