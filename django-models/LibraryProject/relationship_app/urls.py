c5d6816c3346524e5624162ae71b71a12fa53e52
Norwicklee <nomalisa.n1@gmail.com>
2025-03-02 23:25:36 +0200

    Logout errors

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,22 +1,19 @@
-
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
-from . import views
-from .views import list_books
-from .views import LibraryDetailView
-from .views import user_login, user_logout, user_register, home
+from .views import list_books, LibraryDetailView, user_register
 
 def redirect_to_books(request):
-    return redirect('list_books') #or redirect('/relationship/books/')
+    return redirect('list_books')  # or redirect('/relationship/books/')
+
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('', redirect_to_books, name='relationship_home'),
-     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('register/', views.register, name='register'),
-   
+    path('', redirect_to_books, name='home'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # Fixed this line
+    path('register/', user_register, name='register'),
 ]
+
 
 
