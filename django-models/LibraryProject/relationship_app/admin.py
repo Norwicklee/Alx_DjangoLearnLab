bfd583b588fbdbeedee66c71f294026f15f1215a
Norwicklee <nomalisa.n1@gmail.com>
2025-03-02 20:20:48 +0200

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,7 @@
+# relationship_app/admin.py
 from django.contrib import admin
+from .models import Author, Book, Library
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
