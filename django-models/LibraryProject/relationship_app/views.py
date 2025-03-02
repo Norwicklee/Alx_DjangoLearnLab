f2832f9c9023e196734475fc01a62dfa680837d8
Norwicklee <nomalisa.n1@gmail.com>
2025-03-02 21:37:52 +0200

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,45 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Book, Library
-from django.views.generic import DetailView
+from .models import Book
 
-# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    text_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(text_list, content_type="text/plain")
+    return render(request, 'list_books.html', {'books': books})
 
-# Class-based view for Library Details
+from django.views.generic import DetailView
+from .models import Library
+
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
-    context_object_name = "library"
-
-from django.contrib.auth import login, logout
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-
-def user_login(request):
-    if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            login(request, form.get_user())
-            return redirect("list_books")
-    else:
-        form = AuthenticationForm()
-    return render(request, "login.html", {"form": form})
-
-def user_logout(request):
-    logout(request)
-    return render(request, "logout.html")
-
-def register(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("login")
-    else:
-        form = UserCreationForm()
-    return render(request, "register.html", {"form": form})
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
 
